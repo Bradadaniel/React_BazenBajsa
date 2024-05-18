1a24483f0b79c34d36c20e45ceaf560c99aa410c
Bradadaniel <danibrada29@gmail.com>
2024-05-18 11:30:10 +0200

    Fixing Navbar Language Content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { MenuItems } from './MenuItems';
 import './NavbarStyle.css';
 import HunFlag from '../img/hun.png';
 import SrbFlag from '../img/srb.png';
-
 import { useTranslation } from 'react-i18next';
-
 
 const Navbar = () => {
   const [clicked, setClicked] = useState(false);
-  const { i18n } = useTranslation();
+  const { i18n, t } = useTranslation();
 
   const handleClick = () => {
     setClicked(!clicked);
@@ -20,6 +17,45 @@ const Navbar = () => {
     i18n.changeLanguage(lng);
   };
 
+  const MenuItems = [
+    {
+      titleKey: 'navbar.home',
+      url: "/",
+      cName: "nav-links",
+      icon: "fa-solid fa-house-user"
+    },
+    {
+      titleKey: 'navbar.about',
+      url: "/about",
+      cName: "nav-links",
+      icon: "fa-solid fa-circle-info"
+    },
+    {
+      titleKey: 'navbar.pricing',
+      url: "/pricing",
+      cName: "nav-links",
+      icon: "fa-solid fa-dollar-sign"
+    },
+    {
+      titleKey: 'navbar.contact',
+      url: "/contact",
+      cName: "nav-links",
+      icon: "fa-solid fa-phone"
+    },
+    {
+      titleKey: 'navbar.admin',
+      url: "/admin",
+      cName: "nav-links hidden",
+      icon: "fa-solid fa-phone"
+    },
+    {
+      titleKey: 'navbar.login',
+      url: "/login",
+      cName: "nav-links hidden",
+      icon: "fa-solid fa-phone"
+    }
+  ];
+
   return (
     <nav className="navbar-items">
       <h1 className="navbar-logo">BazenBajsa</h1>
@@ -27,16 +63,21 @@ const Navbar = () => {
         <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
       </div>
       <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
-        {MenuItems.map((item, index) => {
-          return (
-            <li key={index}>
-              <Link className={item.cName} to={item.url}><i className={item.icon}></i>{item.title}</Link>
-            </li>
-          );
-        })}
-              <li>
-          <button className='flag' onClick={() => changeLanguage('hu')}><img src={HunFlag} alt="Magyar" /></button>
-          <button className='flag' onClick={() => changeLanguage('sr')}><img src={SrbFlag} alt="Szerb" /> </button>
+        {MenuItems.map((item, index) => (
+          <li key={index}>
+            <Link className={item.cName} to={item.url}>
+              <i className={item.icon}></i>
+              {t(item.titleKey)}
+            </Link>
+          </li>
+        ))}
+        <li>
+          <button className='flag' onClick={() => changeLanguage('hu')}>
+            <img src={HunFlag} alt="Magyar" />
+          </button>
+          <button className='flag' onClick={() => changeLanguage('sr')}>
+            <img src={SrbFlag} alt="Szerb" />
+          </button>
         </li>
       </ul>
     </nav>
